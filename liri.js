5cd5967afdc5f2431e7c57fd6dbdc1285d41ce41
Omar Elhadary <Omar Elhadary>
2016-12-02 01:30:36 -0800

    twitterAPI function showing my last 20 tweets and when they were created

--- liri.js
+++ liri.js
@@ -5,55 +5,44 @@ twitterKeysFile = twitterKeysFile.twitterKeys;
 // second argument will be the song or movie to pass in the function
 var action = process.argv[2];
 var value = process.argv[3];
-
 // switch-case statement to call functions
 switch (action) {
     case "my-tweets":
         twitterAPI();
         break;
-
     case "spotify-this-song":
         spotifyAPI();
         break;
-
     case "movie-this":
         omdbAPI();
         break;
-
     case "do-what-it-says":
         textFile();
         break;
 }
 
 function twitterAPI() {
-
     var twitter = require('twitter');
-
-
     var client = new twitter({
         consumer_key: twitterKeysFile.consumer_key,
         consumer_secret: twitterKeysFile.consumer_secret,
         access_token_key: twitterKeysFile.access_token_key,
         access_token_secret: twitterKeysFile.access_token_secret
     });
-
-    var params = {screen_name: 'omarhadary'};
+    var params = {
+        screen_name: 'omarhadary'
+    };
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
-            console.log(tweets);
+            for (i = 0; i < tweets.length; i++) {
+                console.log("Tweet " + [i + 1] + ": " + tweets[i].text + " ... Created on: " + tweets[i].created_at)
+            };
         }
     });
-
 }
 
-function spotifyAPI() {
+function spotifyAPI() {}
 
-}
+function omdbAPI() {}
 
-function omdbAPI() {
-
-}
-
-function textFile() {
-
-}
+function textFile() {}
