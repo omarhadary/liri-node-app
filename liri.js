e6204c36279a68c441c78ab13d30e74c7d10a369
Omar Elhadary <Omar Elhadary>
2016-12-04 01:58:38 -0800

    do-what-it-says working and completed bonus

--- liri.js
+++ liri.js
@@ -5,20 +5,24 @@ twitterKeysFile = twitterKeysFile.twitterKeys;
 // second argument will be the song or movie to pass in the function
 var action = process.argv[2];
 var songOrMovie = process.argv[3];
-// switch-case statement to call functions
-switch (action) {
-    case "my-tweets":
-        twitterAPI();
-        break;
-    case "spotify-this-song":
-        spotifyAPI();
-        break;
-    case "movie-this":
-        omdbAPI();
-        break;
-    case "do-what-it-says":
-        textFile();
-        break;
+var fs = require("fs");
+
+function LIRICommand() {
+    // switch-case statement to call functions
+    switch (action) {
+        case "my-tweets":
+            twitterAPI();
+            break;
+        case "spotify-this-song":
+            spotifyAPI();
+            break;
+        case "movie-this":
+            omdbAPI();
+            break;
+        case "do-what-it-says":
+            textFile();
+            break;
+    }
 }
 
 function twitterAPI() {
@@ -37,7 +41,13 @@ function twitterAPI() {
         if (!error) {
             //loop through
             for (i = 0; i < tweets.length; i++) {
-                console.log("Tweet " + [i + 1] + ": " + tweets[i].text + " ... Created on: " + tweets[i].created_at)
+                var theStuff = "Tweet " + [i + 1] + ": " + tweets[i].text + " ... Created on: " + tweets[i].created_at;
+                console.log(theStuff);
+                fs.appendFile("log.txt", theStuff + "\n", function(err) {
+                    if (err) {
+                        console.log("Error: " + err);
+                    }
+                });
             };
         }
     });
@@ -45,9 +55,12 @@ function twitterAPI() {
 
 function spotifyAPI() {
     var spotify = require('spotify');
-    spotify.search({ type: 'track', query: songOrMovie }, function(err, data) {
-        if ( err ) {
-           return console.log('Error occurred: ' + err);
+    spotify.search({
+        type: 'track',
+        query: songOrMovie
+    }, function(err, data) {
+        if (err) {
+            return console.log("Error occurred: " + err);
             //if no song is provided
         } else if (songOrMovie == null) {
             songOrMovie = "the sign ace of base";
@@ -55,29 +68,50 @@ function spotifyAPI() {
             return;
         }
         //if song is provided
-        return console.log(JSON.stringify("Artist(s): "+data.tracks.items[0].artists[0].name)+".\nThe song's name: "+data.tracks.items[0].name+".\nA preview link of the song from Spotify: "+data.tracks.items[0].preview_url+"\nThe album that the song is from: "+data.tracks.items[0].album.name+".");
+        var theStuff = JSON.stringify("Artist(s): " + data.tracks.items[0].artists[0].name) + ".\nThe song\'s name: " + data.tracks.items[0].name + ".\nA preview link of the song from Spotify: " + data.tracks.items[0].preview_url + "\nThe album that the song is from: " + data.tracks.items[0].album.name + ".";
+        console.log(theStuff);
+        fs.appendFile("log.txt", "\n----------Spotify Data----------\n" + theStuff, function(err) {
+            if (err) {
+                console.log("Error: " + err);
+            }
+        });
     });
 };
 
 function omdbAPI() {
-
     var request = require("request");
     // Then run a request to the OMDB API with the movie specified
     var queryUrl = "http://www.omdbapi.com/?t=" + songOrMovie + "&y=&plot=short&r=json&tomatoes=true";
-
     request(queryUrl, function(error, response, body) {
-
-        if ( error ) {
-            return console.log('Error occurred: ' + error);
-
+        if (error) {
+            return console.log("Error occurred: " + error);
         } else if (songOrMovie == null) {
             songOrMovie = "Mr. Nobody";
             omdbAPI();
             return;
         }
-            // Parse the body of the site and recover just the imdbRating
-            console.log("Movie Title: "+JSON.parse(body).Title+"\nYear: "+JSON.parse(body).Year+"\nIMDB Rating: "+JSON.parse(body).imdbRating+"\nCountry of Production: "+JSON.parse(body).Country+"\nLanguage: "+JSON.parse(body).Language+"\nPlot: "+JSON.parse(body).Plot+"\nActors: "+JSON.parse(body).Actors+"\nRotten Tomatoes Rating: "+JSON.parse(body).tomatoRating+"\nRotten Tomatoes URL: "+JSON.parse(body).tomatoURL);
+        var theStuff = "Movie Title: " + JSON.parse(body).Title + "\nYear: " + JSON.parse(body).Year + "\nIMDB Rating: " + JSON.parse(body).imdbRating + "\nCountry of Production: " + JSON.parse(body).Country + "\nLanguage: " + JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nActors: " + JSON.parse(body).Actors + "\nRotten Tomatoes Rating: " + JSON.parse(body).tomatoRating + "\nRotten Tomatoes URL: " + JSON.parse(body).tomatoURL;
+        console.log(theStuff);
+        fs.appendFile("log.txt", "\n----------Movie Data----------\n" + theStuff, function(err) {
+            if (err) {
+                console.log("Error: " + err);
+            }
+        });
     });
 };
 
-function textFile() {};
+function textFile() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log("Error occurred: " + error);
+        }
+        // split text file content by comma
+        var textFileArr = data.split(",");
+        // assign the text file 0 index to action and 1 index to songOrMovie
+        action = textFileArr[0];
+        songOrMovie = textFileArr[1];
+        //run the main LIRI function to call the corresponding command
+        LIRICommand();
+    });
+};
+LIRICommand();
